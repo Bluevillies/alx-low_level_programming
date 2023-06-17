142e54df87fdf1c44473004d9cd3cd354f37aba5
Bluevillies <vvarira@gmail.com>
2023-06-17 14:14:28 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints withouth the printf function with a new line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
