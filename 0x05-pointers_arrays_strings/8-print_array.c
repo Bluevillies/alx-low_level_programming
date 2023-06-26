a8a031915da6654d91d08542be187f20ae38e523
Bluevillies <vvarira@gmail.com>
2023-06-26 07:48:13 -0700

    Hate the code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_array - a function that prints n elements of an array
  * @a: array name
@@ -10,13 +9,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d,", a[i]);
-	}
-		if (i == (n - 1))
-		{
-			printf("%d", a[n - 1]);
+		printf("%d", a[i]);
+		if (i != (n - 1))
+	{
+		printf(", ");
 		}
-			printf("\n");
+	}
+		printf("\n");
 }
