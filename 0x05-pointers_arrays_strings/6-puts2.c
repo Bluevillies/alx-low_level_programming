2047a3fbe0924cc62df4bafce2b5513b81e92cb3
Bluevillies <vvarira@gmail.com>
2023-06-26 06:49:44 -0700

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@
  */
 void puts2(char *str)
 {
-	int longi = o;
+	int longi = 0;
 	int t = 0;
 	char *y = str;
 	int o;
@@ -20,9 +20,9 @@ void puts2(char *str)
 	t = longi - 1;
 	for (o = 0; o <= t; o++)
 	{
-		if (o % 2 ==0)
+		if (o % 2 == 0)
 	{
-		_putschar(str[o]);
+		_putchar(str[o]);
 	}
 	}
 	_putchar('\n');
