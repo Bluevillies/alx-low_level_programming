c750af9a6c263da0fa78288ac161596b301d7bfb
Bluevillies <vvarira@gamil.com>
2023-09-05 15:19:20 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,22 +1,21 @@
 #include <stdlib.h>
-#include "main.h"		
+#include "main.h"
 /**
  *  * count_word - helper function to count the number of words in a string
- *   * @s: string to evaluate	
+ *   * @s: string to evaluate
  *    *
  *     * Return: number of words
  *      */
+
 int count_word(char *s)
 {
-		int flag, c, w;
-		
-			flag = 0;
-		
-				w = 0;
+	int flag, c, w;
 
-					for (c = 0; s[c] != '\0'; c++)
-							{
-			if (s[c] == ' ')
+			flag = 0;
+				w = 0;
+	for (c = 0; s[c] != '\0'; c++)
+			{
+	if (s[c] == ' ')
 					flag = 0;											else if (flag == 0)													{														flag = 1;															w++;
 															}												}
 						return (w);		
@@ -57,7 +56,7 @@ char **strtow(char *str)
 				matrix[k] = tmp - c;
 				k++;
 				c = 0;
-			}
+		}
 		}
 		else if (c++ == 0)
 			start = i;
