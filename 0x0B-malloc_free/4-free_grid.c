9129e49c7ba996166200c6b3478af6fb624ef13c
Bluevillies <vvarira@gamil.com>
2023-09-05 14:42:59 -0700

    It is not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>		
+#include <stdlib.h>
 #include "main.h"
 /**
- * free_grid - frees 2d array		
+ * free_grid - frees 2d array
  * @grid: 2d grid
  * @height: height dimension of grid
  * Description: frees memory of grid
@@ -13,7 +13,7 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-for (i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
